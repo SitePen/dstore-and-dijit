16bd6b572c5f2da928f51d08b4ad990866fc760a
Brian Weiser <bweiser@sitepen.com>
2015-07-27 16:26:10 -0700

    add demo

---
+++ custom/widget/ListWidget.js
@@ -0,0 +1,32 @@
+define([
+	'dojo/_base/declare',
+	'dojo/dom-construct',
+	'dojo/on',
+	'dojo/_base/lang',
+
+	'dijit/_WidgetBase',
+	'custom/store/_DstoreDijitMixin',
+
+	'dojo/query'
+], function (declare, domConstruct, on, lang, _WidgetBase, _DstoreDijitMixin) {
+	return declare([_WidgetBase, _DstoreDijitMixin], {
+		postCreate: function () {
+			this.inherited(arguments);
+
+			on(this, 'div:click', lang.hitch(this, 'removeItemFromStore'));
+		},
+
+		removeItemFromStore: function (event) {
+			var id = parseFloat(event.target.id, 10);
+
+			this.store.remove(id);
+		},
+
+		_renderItem: function (item, index) {
+			return domConstruct.create('div', {
+				textContent: item.text,
+				id: item.id
+			});
+		}
+	});
+});
